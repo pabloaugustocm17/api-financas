53eefd596f7d2dff4dad9ed8ee7902882f977a6f
Pablo Magalhães <109962972+pabloaugustocm17@users.noreply.github.com>
2023-07-10 20:57:51 -0300

    feature: create user repository
    - create user repository and add method 'findUserByEmail()'

---
+++ app/src/main/java/com/app/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.app.repositories;
+
+import com.app.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    
+    Optional<User> findUserByEmail(String email);
+
+}
