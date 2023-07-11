f61edd3b3a115704d03e9c26b35f9dade39c3e72
Pablo Magalhães <109962972+pabloaugustocm17@users.noreply.github.com>
2023-07-10 21:19:13 -0300

    feature: query login

--- app/src/main/java/com/app/repositories/UserRepository.java
+++ app/src/main/java/com/app/repositories/UserRepository.java
@@ -2,6 +2,7 @@ package com.app.repositories;
 
 import com.app.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,5 +12,14 @@ import java.util.UUID;
 public interface UserRepository extends JpaRepository<User, UUID> {
     
     Optional<User> findUserByEmail(String email);
+
+    @Query( "SELECT U " +
+            "FROM User U " +
+            "WHERE U.email = :email " +
+            "AND U.password = :password")
+    Optional<User> _realizeLogin(
+            String email,
+            String password
+    );
 
 }
