e610c41d3aa1cd0d33bf2178f5af4f997529b369
Pablo Magalhães <109962972+pabloaugustocm17@users.noreply.github.com>
2023-07-11 19:49:05 -0300

    feature: add query in tokenRepository
    - query to find user by token access

--- app/src/main/java/com/app/repositories/TokenRepository.java
+++ app/src/main/java/com/app/repositories/TokenRepository.java
@@ -1,7 +1,9 @@
 package com.app.repositories;
 
 import com.app.models.Token;
+import com.app.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,5 +14,11 @@ public interface TokenRepository extends JpaRepository<Token, UUID> {
 
     Optional<Token> findTokenByEmail(String email);
 
+    @Query( "SELECT U " +
+            "FROM Token T " +
+            "INNER JOIN User U " +
+            "ON U.email = T.email " +
+            "WHERE T.id = :token")
+    Optional<User> _findUserByToken(UUID token);
 
 }
