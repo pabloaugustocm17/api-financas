53219b0f0c989cad45cedebf267cb34a559eed51
Pablo Magalhães <109962972+pabloaugustocm17@users.noreply.github.com>
2023-07-11 00:00:15 -0300

    feature: implement table token

---
+++ app/src/main/java/com/app/models/Token.java
@@ -0,0 +1,30 @@
+package com.app.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "token")
+public class Token {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String email;
+
+    private LocalDateTime last_access;
+
+    public Token(){}
+
+    public Token(String email) {
+        this.email = email;
+        this.last_access = LocalDateTime.now();
+    }
+
+    public UUID _getId(){
+        return this.id;
+    }
+}
