a7c48df4e8652ebf8f27c156c9095902309a365c
Pablo Magalhães <109962972+pabloaugustocm17@users.noreply.github.com>
2023-08-08 19:46:43 -0300

    feat: create query

--- app/src/main/java/com/app/repositories/WalletRepository.java
+++ app/src/main/java/com/app/repositories/WalletRepository.java
@@ -3,6 +3,8 @@ package com.app.repositories;
 import com.app.models.User;
 import com.app.models.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,6 +13,11 @@ import java.util.UUID;
 @Repository
 public interface WalletRepository extends JpaRepository<Wallet, UUID> {
 
-    Optional<Wallet> findWalletByUser(User user);
+    @Query(" SELECT W " +
+            "FROM Wallet AS W " +
+            "WHERE W.user_id.id = :id")
+    Optional<Wallet> returnWalletByUserId(
+            @Param("id") UUID id
+    );
 
 }
